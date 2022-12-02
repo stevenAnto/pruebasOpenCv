609aa8a116069f523c345f2cf1a3695375556319
Esteven <ecalcinap@unsa.edu.pe>
2022-12-01 20:16:21 -0500

    solo detecta pequenias areas de color azul

--- Tracking.py
+++ Tracking.py
@@ -13,11 +13,15 @@ while True:
         #cambiamos a espacio HSV
         frameHSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(frameHSV,azulBajo,azulAlto)
-        a,contornos, = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+        contornos,sinUso, = cv2.findContours(mask, cv2.RETR_EXTERNAL,
                                         cv2.CHAIN_APPROX_SIMPLE)
+        for c in contornos:
+            area = cv2.contourArea(c)
+            if area > 3000:
+                #se pone 0 solo para dibujar ciertos contornos
+                cv2.drawContours(frame,[c],0,(255,0,0),3)
 
-
-        cv2.drawContours(frame,a,-1,(255,0,0),3)# Dibjuamos los contornos en frame con -1 se bijutan todos los contoros, en azul con grosor 3
+        #cv2.drawContours(frame,a,-1,(255,0,0),3)# Dibjuamos los contornos en frame con -1 se bijutan todos los contoros, en azul con grosor 3
         #cv2.imshow('maskAzul',mask)
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('d'):
