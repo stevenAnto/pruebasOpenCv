8676ddb53428d307d217eacfe5bc05228ba76bf8
Esteven <ecalcinap@unsa.edu.pe>
2022-12-01 21:01:24 -0500

    Tracing de los tres colores

--- Tracking.py
+++ Tracking.py
@@ -1,6 +1,24 @@
 import cv2
 import numpy as np
 
+def dibujar(mask,color):
+    contornos, sinUso, = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+                                          cv2.CHAIN_APPROX_SIMPLE)
+    for c in contornos:
+        area = cv2.contourArea(c)
+        if area > 3000:
+            # Buscamos el centro
+            M = cv2.moments(c)
+            if (M["m00"] == 0): M["m00"] = 1
+            x = int(M["m10"] / M["m00"])
+            y = int(M["m01"] / M["m00"])
+            cv2.circle(frame, (x, y), 7, (0, 255, 0), -1)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, '{},{}'.format(x, y), (x + 10, y), font, 0.75, (0, 255, 0), 1, cv2.LINE_AA)
+            nuevoContorno = cv2.convexHull(c)
+            # se pone 0 solo para dibujar ciertos contornos
+            cv2.drawContours(frame, [nuevoContorno], 0, color, 4)
+
 cap = cv2.VideoCapture(0)
 #Sacado de StackOverFlow
 azulBajo = np.array([100,100,20],np.uint8)
@@ -15,32 +33,20 @@ redAlto1 = np.array([5,255,255],np.uint8)
 redBajo2 = np.array([0,100,20],np.uint8)
 redAlto2 = np.array([5,255,255],np.uint8)
 
-
+font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
     ret, frame = cap.read()
     if ret == True:
         #cambiamos a espacio HSV
         frameHSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(frameHSV,azulBajo,azulAlto)
-        contornos,sinUso, = cv2.findContours(mask, cv2.RETR_EXTERNAL,
-                                        cv2.CHAIN_APPROX_SIMPLE)
-        for c in contornos:
-            area = cv2.contourArea(c)
-            if area > 3000:
-                #Buscamos el centro
-                M = cv2.moments(c)
-                if (M["m00"]==0): M["m00"]=1
-                x=int(M["m10"]/M["m00"])
-                y=int(M["m01"]/M["m00"])
-                cv2.circle(frame,(x,y),7,(0,255,0),-1)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(frame,'{},{}'.format(x,y),(x+10,y),font,0.75,(0,255,0),1,cv2.LINE_AA)
-                nuevoContorno =cv2.convexHull(c)
-                #se pone 0 solo para dibujar ciertos contornos
-                cv2.drawContours(frame,[nuevoContorno],0,(255,0,0),3)
-
-        #cv2.drawContours(frame,a,-1,(255,0,0),3)# Dibjuamos los contornos en frame con -1 se bijutan todos los contoros, en azul con grosor 3
-        #cv2.imshow('maskAzul',mask)
+        maskAzul = cv2.inRange(frameHSV,azulBajo,azulAlto)
+        maskAmarillo = cv2.inRange(frameHSV, amarilloBajo, amarilloAlto)
+        maskRed1 = cv2.inRange(frameHSV, redBajo1, redAlto1)
+        maskRed2 = cv2.inRange(frameHSV, redBajo2, redAlto2)
+        maskRed = cv2.add(maskRed1,maskRed2)
+        dibujar(maskAzul,(255,0,0))
+        dibujar(maskAmarillo,(0,255,255))
+        dibujar(maskRed,(0,0,255))
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('d'):
             break
