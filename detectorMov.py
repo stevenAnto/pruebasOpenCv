35c4c8bff1498c2e963b6c2c4f4c1dc6c8c004b9
Esteven <ecalcinap@unsa.edu.pe>
2022-12-02 23:42:52 -0500

    se termina el detector de movimiento

--- detectorMov.py
+++ detectorMov.py
@@ -14,7 +14,18 @@ while True:
         bgGray = gray
     if i> 20:
         dif =cv2 .absdiff(gray,bgGray)
-        cv2.imshow( 'diff',dif)
+        #cambiamos a imagene binaria, mediante umbralizacion
+        sinUsar,th = cv2.threshold(dif,40,255,cv2.THRESH_BINARY)
+        contornos = cv2.findContours(th, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
+        #cv2.drawContours(frame,contornos,-1,(0,0,255),2)
+        #cv2.imshow( 'diff',dif)
+        cv2.imshow('th', th)
+        #dibujamos rectangulos en los contorndos encontrados
+        for c in contornos:
+            area =cv2.contourArea(c)
+            if area >7000:
+                x,y,w,h = cv2.boundingRect(c)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
     cv2.imshow('Frame',frame)
     i=i+1
     if cv2.waitKey(1) & 0XFF ==ord('d'):
