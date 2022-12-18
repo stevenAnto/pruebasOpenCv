abf582cd409f46de37f061f244b92f0686646bc3
Esteven <ecalcinap@unsa.edu.pe>
2022-12-18 17:56:33 -0500

    dibujo contorno de la umbralizacion

--- ContadorDedos.py
+++ ContadorDedos.py
@@ -23,11 +23,14 @@ bg = None
 
 while(captura.isOpened()):
     #devuelve un booleano y la image
+
     ret, image = captura.read()
+
     if ret==True:
 
         image =imutils.resize(image,width=600)# redimensione las capturas
         image = cv2.flip(image,1) #se invierte par ano tener confusion
+        cv2.rectangle(image, (20, 20), (20 + 200, 20 + 300), color_contorno, 4)
 
         imageAux = image.copy()
         if bg is not None:
@@ -43,7 +46,13 @@ while(captura.isOpened()):
             #aplicando umbralizacion para separar el fondo
             dif = cv2.absdiff(grayFragmento,bgFragmento)
             th =cv2.threshold(dif,30,255,cv2.THRESH_BINARY)[1]
-            cv2.imshow("dif",dif)
+            th = cv2.medianBlur(th, 7)#ajuste para mejor umbralizacion
+            contornos = cv2.findContours(th, cv2.RETR_EXTERNAL,
+                                         cv2.CHAIN_APPROX_SIMPLE)[0]
+            contornos = sorted(contornos,key=cv2.contourArea,reverse=True)[:1]
+            cv2.drawContours(fragmento, contornos, 0, (0,255,0), 3)
+
+            #cv2.imshow("dif",dif)
             cv2.imshow("th",th)
 
         cv2.imshow('video', image)
